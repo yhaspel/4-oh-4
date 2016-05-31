12bd18f6e9d1bb57bf37b3bdba7d3ea4e8bdbc1b
Yoni Mood <yonispark@gmail.com>
2016-05-31 21:31:57 +0300

    login logout work but not validated

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.views.generic import View
+from django.views.generic.edit import FormView
+from . import forms
+from django.shortcuts import redirect
 
-# Create your views here.
+
+class LoginView(FormView):
+    form_class = forms.LoginForm
+    template_name = "authentication/login.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return redirect('emessages:home')
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        user = authenticate(username=form.cleaned_data['username'],
+                            password=form.cleaned_data['password'])
+
+        if user is not None and user.is_active:
+            login(self.request, user)
+            if self.request.GET.get('from'):
+                return redirect(
+                    self.request.GET['from'])  # SECURITY: check path
+            return redirect('emessages:home')
+
+        form.add_error(None, "Invalid user name or password")
+        return self.form_invalid(form)
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect("authentication:login")
--- fourohfour/urls.py
+++ fourohfour/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include("emessages.urls")),
+    url(r'^', include("authentication.urls")),
     url(r'^admin/', admin.site.urls),
 ]
