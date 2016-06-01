ccc04a6aff1db993c4fbf3580e62fdee7764fb83
Yoni Mood <yonispark@gmail.com>
2016-06-01 13:49:56 +0300

    e mmessage .image .description can be nullabe blank

--- emessages/models.py
+++ emessages/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class EMessage(models.Model):
     title = models.CharField(max_length=40)
-    description = models.TextField(max_length=1000)
-    img_url = models.CharField(max_length=500, name='image URL')
+    description = models.TextField(max_length=1000, null=True, blank=True)
+    img_url = models.CharField(max_length=500, name='image URL', null=True, blank=True)
     error_code = models.CharField(max_length=6)
 
     def __str__(self):
-        return "{}".format(self.title)
+        return "EMessage.;{}".format(self.title)
