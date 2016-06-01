c084a17736d74a061736d79d4700ce6d7015bb67
yhaspel <yuval3000@gmail.com>
2016-06-01 13:37:22 +0300

    errors with reverse lookup, added Bootstrap to INSTALLED APP. only /add seems to work

--- fourohfour/settings.py
+++ fourohfour/settings.py
@@ -35,6 +35,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party or any other party
+    'django_extensions',
+    'bootstrap3',
+
+    # our very own children
     'emessages',
     'authentication',
 ]
