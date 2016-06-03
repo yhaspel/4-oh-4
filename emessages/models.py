22a0d33c4adb7970f3a9fecd6ae011542080d0c1
yhaspel <yuval3000@gmail.com>
2016-06-03 18:51:20 +0300

    added remove an error message from list with prompt form

--- emessages/models.py
+++ emessages/models.py
@@ -6,7 +6,7 @@ from fourohfour.settings import MEDIA_URL
 class EMessage(models.Model):
     title = models.CharField(max_length=40)
     description = models.TextField(max_length=1000, null=True, blank=True)
-    error_code = models.CharField(max_length=6)
+    error_code = models.CharField(max_length=6, primary_key=True)
     photo = models.ImageField(upload_to="error_images", default="", null=True, blank=True)
 
     def __str__(self):
