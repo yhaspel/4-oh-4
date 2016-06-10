adf99b5498ce0b5f1f1f5a0ca2f0eefc179cf86e
MoodyUser <yonispark@gmail.com>
2016-06-10 19:17:17 +0300

    Delete local_settings.py

--- fourohfour/local_settings.py
+++
@@ -1,8 +0,0 @@
-from .base_settings import *
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l=$7#w-e)3gmus_1y%ejt^b12pa=0h-3hv(gzo3jnsf2kt(s4f'
-
-DEBUG = True
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
